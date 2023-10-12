d25ffe7bffd0841e41ebcb05a6d8e4dfeb0eab36
승희 <141980284+seunghee1108@users.noreply.github.com>
2023-10-12 10:57:21 +0900

    향마루 응답처리

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {"content-Type" : "text/plain"});
+  res.write("향마루");
+  res.end();
+  // plain : 설명
+  // 향마루를 설명
+});
+
+server.listen(3500, (err) => {
+  if(!err) {
+    console.error('서버 안돌어감');
+  } else {
+    console.log("3500 포트에 돌아감 http://localhost:3500");
+  }
+});
