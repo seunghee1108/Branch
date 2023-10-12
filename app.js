06d765f13fac9b85e7d83547ec1e560874b84778
승희 <141980284+seunghee1108@users.noreply.github.com>
2023-10-12 11:04:14 +0900

    확인

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3500, (err) => {
-  if(!err) {
-    console.error('서버 안돌어감');
-  } else {
-    console.log("3500 포트에 돌아감 http://localhost:3500");
-  }
+  // if(!err) {
+  //   console.error('서버 안돌어감');
+  // } else {
+  //   console.log("3500 포트에 돌아감 http://localhost:3500");
+  // }
 });
